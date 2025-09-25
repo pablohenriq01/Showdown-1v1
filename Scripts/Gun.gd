43ac7f2e438010458851648d909730e157700f40
PabloH <pablohenriqq01@gmail.com>
2025-09-25 17:42:02 -0300

    criação das munições testes | Script e o prefab

--- Scripts/Gun.gd
+++ Scripts/Gun.gd
@@ -1,16 +1,26 @@
-extends Node
+extends Node2D
 class_name Gun
 
 @export_category("Gun Settings")
 @export var gun_offset_x: float = 20.0
 @export var gun_offset_y: float = 0.0
 
+var bullet_path=preload("res://Prefabs/Bullets/bullet_test.tscn")
+func _physics_process(delta):
+	
+	look_at(get_global_mouse_position())
+	if Input.is_action_just_pressed("ui_accept"):
+		fire()
 func _ready() -> void:
 
 	adjust_offset()
 
 	pass 
-
+func fire():
+	var bullet = bullet_path.instantiate()
+	bullet.dir=rotation
+	bullet.pos=$".".global_position
+	get_parent().add_child(bullet)
 
 func _process(delta: float) -> void:
 	pass
